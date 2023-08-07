1236b0e3b27a676c94bb6ae3dc42088263966314
aniketverma19233 <138116136+aniketverma19233@users.noreply.github.com>
2023-08-07 10:14:02 +0530

    Update load_artifacts.js

--- load_artifacts.js
+++ load_artifacts.js
@@ -17,7 +17,7 @@ const loadScript = (FILE_URL, async = true, type = "text/javascript") => {
                 });
             });
 
-            document.head.appendChild(scriptEle);
+            document.body.appendChild(scriptEle);
         } catch (error) {
             reject(error);
         }
