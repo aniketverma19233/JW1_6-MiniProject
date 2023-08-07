da8976c79fd41a35fceaf8083ca8c771d3584410
aniketverma19233 <138116136+aniketverma19233@users.noreply.github.com>
2023-08-07 17:00:00 +0530

    Add files via upload

---
+++ load_artifactx.js
@@ -0,0 +1,35 @@
+const loadScript = (FILE_URL, async = true, type = "text/javascript") => {
+    return new Promise((resolve, reject) => {
+        try {
+            const scriptEle = document.createElement("script");
+            scriptEle.type = type;
+            scriptEle.src =FILE_URL;
+
+            scriptEle.addEventListener("load", (ev) => {
+                resolve({ status: true });
+            });
+
+            scriptEle.addEventListener("error", (ev) => {
+                reject({
+                    status: false,
+                    message: `Failed to load the script ＄{FILE_URL}`
+                });
+            });
+
+            document.head.appendChild(scriptEle);
+            var DOMContentLoaded_event = document.createEvent("Event");
+	    DOMContentLoaded_event.initEvent("DOMContentLoaded", true, true);
+	    window.document.dispatchEvent(DOMContentLoaded_event);
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+loadScript("https://cdn.jsdelivr.net/gh/aniketverma19233/JW1_6-MiniProject@latest/tcx.js").then( data  => {
+        console.log("Script loaded successfully", data);
+    })
+    .catch( err => {
+        console.error(err);
+    });
+
