236fc38e0d07a3f6705fd1dc599b5d8321b0b15f
aniketverma19233 <138116136+aniketverma19233@users.noreply.github.com>
2023-08-07 10:24:08 +0530

    Update load_artifacts.js

--- load_artifacts.js
+++ load_artifacts.js
@@ -17,7 +17,7 @@ const loadScript = (FILE_URL, async = true, type = "text/javascript") => {
                 });
             });
 
-            document.head.appendChild(scriptEle);
+            document.getElementById("xml-editor-app").appendChild(scriptEle);
         } catch (error) {
             reject(error);
         }
